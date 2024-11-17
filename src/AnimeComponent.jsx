b14b9c5a0ee1b7dd41c966eae3014f4d065d604b
carlos <comtechy@outlook.com>
2024-11-17 14:28:38 -0500

    Added more cleanup code

--- src/AnimeComponent.jsx
+++ src/AnimeComponent.jsx
@@ -3,7 +3,13 @@ import { useContext } from "react";
 import AnimeReactContext from "./AnimeReactProviderContext";
 
 const animeAtom = atom('not set');
-animeAtom.debugLabel = ">>> Anime Atom";
+animeAtom.onMount = () => {
+  console.log(`>>> ${Date.now()}: anximeAtom mounted.`);
+  return () => {
+    console.log(`>>> ${Date.now()}: anximeAtom UN-mounted.`);
+  };
+};
+animeAtom.debugLabel = ">>> debugLabel: Anime Atom";
 
 export const AnimeComponent = ({ componentId }) => {
   const [anime, setAnime] = useAtom(animeAtom);
